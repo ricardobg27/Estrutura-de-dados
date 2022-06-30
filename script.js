396c72d61776cd7c629a0c5ae55fa17480259d18
Ricardo Borges <ricardoborges00@hotmail.com>
2022-06-30 17:40:37 -0300

    finalizado

---
+++ script.js
@@ -0,0 +1,18 @@
+let nome = "Ricardo"
+console.log(nome)
+
+let number = 22
+console.log(number)
+
+let ano = 2022
+let menos = ano - number 
+console.log(menos)
+
+console.log(ano + number)
+
+console.log("estou mexendo no console.log")
+
+let quartaFeira = true 
+console.log(quartaFeira)
+
+console.log(typeof quartaFeira)
